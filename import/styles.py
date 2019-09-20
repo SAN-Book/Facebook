3d4819cf7f5ac6c70dac0b1e2674550d551ebb1c
Susanto <50844272+M3DPR0@users.noreply.github.com>
2019-09-20 10:14:06 +0700

    Create styles.py

---
+++ import/styles.py
@@ -0,0 +1,55 @@
+#Build Status PyPI version
+
+#Style is a simple terminal string styling package. 
+#Its API is a port of the popular chalk package for javascript.
+
+#Install
+$ pip install style
+#Usage
+import style
+print(style.red('SAN', style.bold('BOOK') + '!'))
+
+#Modifiers
+bold
+dim
+italic
+underline
+inverse
+hidden
+strikethrough
+
+#Colors
+black
+red
+green
+yellow
+blue
+magenta
+cyan
+white
+
+#Background colors
+light_black
+light_red
+light_green
+light_yellow
+light_blue
+light_magenta
+light_cyan
+light_white
+on_black
+on_red
+on_green
+on_yellow
+on_blue
+on_magenta
+on_cyan
+on_white
+on_light_black
+on_light_red
+on_light_green
+on_light_yellow
+on_light_blue
+on_light_magenta
+on_light_cyan
+on_light_white
